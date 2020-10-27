1ed1648a29c54e5f8ca7e2d907fc7aa67f0b0181
l-sousa <joselmdbsousa@ua.pt>
2020-10-27 12:11:29 +0000

    fix

--- tree_search.py
+++ tree_search.py
@@ -1,6 +1,5 @@
-
 # Module: tree_search
-# 
+#
 # This module provides a set o classes for automated
 # problem solving through tree search:
 #    SearchDomain  - problem domains
@@ -17,6 +16,8 @@ from abc import ABC, abstractmethod
 # Dominios de pesquisa
 # Permitem calcular
 # as accoes possiveis em cada estado, etc
+
+
 class SearchDomain(ABC):
 
     # construtor
@@ -57,57 +58,77 @@ class SearchProblem:
         self.domain = domain
         self.initial = initial
         self.goal = goal
+
     def goal_test(self, state):
-        return self.domain.satisfies(state,self.goal)
+        return self.domain.satisfies(state, self.goal)
+
 
 # Nos de uma arvore de pesquisa
 class SearchNode:
-    def __init__(self,state,parent): 
+    def __init__(self, state, parent, depth=0):
         self.state = state
         self.parent = parent
+        self.depth = depth
+
     def __str__(self):
         return "no(" + str(self.state) + "," + str(self.parent) + ")"
+
     def __repr__(self):
         return str(self)
+
 
 # Arvores de pesquisa
 class SearchTree:
 
     # construtor
-    def __init__(self,problem, strategy='breadth'): 
+    def __init__(self, problem, strategy="breadth"):
         self.problem = problem
         root = SearchNode(problem.initial, None)
         self.open_nodes = [root]
         self.strategy = strategy
+        self.solution = None
+        self.terminals = 0
+        self.non_terminals = 0
 
     # obter o caminho (sequencia de estados) da raiz ate um no
-    def get_path(self,node):
+    def get_path(self, node):
         if node.parent == None:
             return [node.state]
         path = self.get_path(node.parent)
         path += [node.state]
-        return(path)
+        return path
+
+    def length(self):
+        return self.solution.depth
 
     # procurar a solucao
-    def search(self):
+
+    def search(self, limit=None):
         while self.open_nodes != []:
             node = self.open_nodes.pop(0)
             if self.problem.goal_test(node.state):
+                self.solution = node  # define como solution o node que deu bem
                 return self.get_path(node)
             lnewnodes = []
             for a in self.problem.domain.actions(node.state):
-                newstate = self.problem.domain.result(node.state,a)
-                newnode = SearchNode(newstate,node)
-                lnewnodes.append(newnode)
+                newstate = self.problem.domain.result(node.state, a)
+                # aumenta o depth count
+                newnode = SearchNode(newstate, node, node.depth + 1)
+                # para não repetir estados
+                if newstate not in self.get_path(node) and (limit is None or newnode.depth < limit):
+                    lnewnodes.append(newnode)
+
             self.add_to_open(lnewnodes)
+
+        self.non_terminals = len(lnewnodes)
+
         return None
 
     # juntar novos nos a lista de nos abertos de acordo com a estrategia
-    def add_to_open(self,lnewnodes):
-        if self.strategy == 'breadth':
+    def add_to_open(self, lnewnodes):
+        if self.strategy == "breadth":
             self.open_nodes.extend(lnewnodes)
-        elif self.strategy == 'depth':
+        elif self.strategy == "depth":
             self.open_nodes[:0] = lnewnodes
-        elif self.strategy == 'uniform':
+        elif self.strategy == "uniform":
             pass
-
